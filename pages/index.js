0e0a711990e1e3c8c49da45c71298370e75d1736
ryusuke <nakamura.r@tam-tam.co.jp>
2021-08-04 16:53:48 +0900

    コミットテストでテキスト追加

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home({ blog }) {
         {blog.map((blog) => (
           <li key={blog.id}>
             <Link href={`/blog/${blog.id}`}>
-              <a>{blog.title}</a>
+              <a>{blog.title}あああ</a>
             </Link>
           </li> 
         ))}
